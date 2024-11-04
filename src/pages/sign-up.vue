a2515e3a99935f403e7b594b0bc0204446a5cf40
Bekjon Ishpulatov <bekjon.ishpulatov@each.uz>
2024-11-04 14:05:27 +0500

    feat: add auth service

--- src/pages/sign-up.vue
+++ src/pages/sign-up.vue
@@ -1,5 +1,22 @@
-<script setup>
+<script setup lang="ts">
+  import type { RegisterPayload } from "@/service/auth-service"
+  import AuthService from "@/service/auth-service"
 
+  const payload = reactive<RegisterPayload>({
+    username: "",
+    first_name: "",
+    last_name: "",
+    password: "",
+  })
+
+  async function onSubmit() {
+    try {
+      await AuthService.register(payload)
+    }
+    catch (error: any) {
+      console.error(error)
+    }
+  }
 </script>
 
 <template>
@@ -12,32 +29,32 @@
 
     <div class="mt-4 sm:mx-auto sm:w-full sm:max-w-md">
       <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-        <form class="space-y-6" action="#" method="POST">
+        <form class="space-y-6" @submit.prevent="onSubmit">
           <div class="ml-0">
             <label for="username" class="block text-sm font-medium text-gray-700">Username</label>
             <div class="mt-1">
-              <input id="username" name="username" placeholder="username" type="text" autocomplete="username" required="" class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
+              <input id="username" v-model="payload.username" name="username" placeholder="username" type="text" autocomplete="username" required class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
             </div>
           </div>
 
           <div>
             <label for="firstname" class="block text-sm font-medium text-gray-700">First Name</label>
             <div class="mt-1">
-              <input id="firstname" name="firstname" placeholder="first_name" type="text" autocomplete="firstname" required="" class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
+              <input id="firstname" v-model="payload.first_name" name="firstname" placeholder="first_name" type="text" autocomplete="firstname" required class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
             </div>
           </div>
 
           <div>
             <label for="lastname" class="block text-sm font-medium text-gray-700">Last Name</label>
             <div class="mt-1">
-              <input id="lastname" name="lastname" placeholder="last_name" type="text" autocomplete="lastname" required="" class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
+              <input id="lastname" v-model="payload.last_name" name="lastname" placeholder="last_name" type="text" autocomplete="lastname" required class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
             </div>
           </div>
 
           <div>
             <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
             <div class="mt-1">
-              <input id="password" placeholder="password" name="password" type="password" autocomplete="current-password" required="" class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
+              <input id="password" v-model="payload.password" placeholder="password" name="password" type="password" autocomplete="current-password" required class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
             </div>
           </div>
 
@@ -48,7 +65,7 @@
             </div>
 
             <div class="text-sm">
-              <RouterLink to="/sign-in" class="font-medium text-indigo-600 hover:text-indigo-500">
+              <RouterLink to="/" class="font-medium text-indigo-600 hover:text-indigo-500">
                 Login
               </RouterLink>
             </div>
